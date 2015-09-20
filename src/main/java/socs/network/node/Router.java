68d6d6340a6df4e5b5781810c623ebdc44d5435b
Daniel Pham <pham.dany@gmail.com>
2015-09-20 13:24:13 -0400

    Partial for Assignement 1

--- src/main/java/socs/network/node/Router.java
+++ src/main/java/socs/network/node/Router.java
@@ -4,16 +4,18 @@ import socs.network.util.Configuration;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class Router {
-
     protected LinkStateDatabase lsd;
 
     RouterDescription rd = new RouterDescription();
 
     //assuming that all routers are with 4 ports
-    Link[] ports = new Link[4];
+    // Link[] ports = new Link[4];
+    List<Link> ports = new ArrayList<Link>(4);
 
     public Router(Configuration config) {
         rd.simulatedIPAddress = config.getString("socs.network.router.ip");
@@ -38,7 +40,13 @@ public class Router {
      * @param portNumber the port number which the link attaches at
      */
     private void processDisconnect(short portNumber) {
+        for (Link l : ports) {
+            if (l.router2.processPortNumber == portNumber) {
+                ports.remove(l);
 
+                // TODO: Announce change.
+            }
+        }
     }
 
     /**
@@ -49,7 +57,22 @@ public class Router {
      * NOTE: this command should not trigger link database synchronization
      */
     private void processAttach(String processIP, short processPort, String simulatedIP, short weight) {
+        if (this.ports.size() >= 4) {
+            System.out.println(this.rd.simulatedIPAddress + " is at capacity.");
+            return;
+        }
 
+        RouterDescription newRd = new RouterDescription();
+
+        newRd.processIPAddress = processIP;
+        newRd.processPortNumber = processPort;
+        newRd.simulatedIPAddress = simulatedIP;
+        newRd.status = RouterStatus.INIT;
+
+        Link newLink = new Link(this.rd, newRd);
+        ports.add(newLink);
+
+        // TODO: Add info to LinkStateDatabase
     }
 
     /**
@@ -74,14 +97,27 @@ public class Router {
      * output the neighbors of the routers
      */
     private void processNeighbors() {
+        System.out.println("Neighbors of " + this.rd.simulatedIPAddress + ":");
 
+        for (Link l : this.ports) {
+            if (l == null) continue;
+
+            // By construction, self router is router1 in its own ports list.
+            System.out.println("IP Address: " + l.router2.simulatedIPAddress);
+        }
     }
 
     /**
      * disconnect with all neighbors and quit the program
      */
     private void processQuit() {
+        // TODO: Announce this router is quitting.
+        for (Link l : this.ports) {
+            this.ports.remove(l);
 
+        }
+
+        System.out.println("Process has quit succesfully.");
     }
 
     public void terminal() {
@@ -94,27 +130,35 @@ public class Router {
                 if (command.startsWith("detect ")) {
                     String[] cmdLine = command.split(" ");
                     processDetect(cmdLine[1]);
+
                 } else if (command.startsWith("disconnect ")) {
                     String[] cmdLine = command.split(" ");
                     processDisconnect(Short.parseShort(cmdLine[1]));
+
                 } else if (command.startsWith("quit")) {
                     processQuit();
+                    break;
+
                 } else if (command.startsWith("attach ")) {
                     String[] cmdLine = command.split(" ");
                     processAttach(cmdLine[1], Short.parseShort(cmdLine[2]),
                             cmdLine[3], Short.parseShort(cmdLine[4]));
+
                 } else if (command.equals("start")) {
                     processStart();
+
                 } else if (command.equals("connect ")) {
                     String[] cmdLine = command.split(" ");
                     processConnect(cmdLine[1], Short.parseShort(cmdLine[2]),
                             cmdLine[3], Short.parseShort(cmdLine[4]));
+
                 } else if (command.equals("neighbors")) {
                     //output neighbors
                     processNeighbors();
+
                 } else {
                     //invalid command
-                    break;
+                    System.out.println("Invalid command");
                 }
                 System.out.print(">> ");
                 command = br.readLine();
